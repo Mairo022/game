843a68b27046d1566d260cf5befe28319faf8a6d
Mairo022 <mairo022@outlook.com>
2026-06-25 16:46:07 +0300

    Events file

---
+++ events.js
@@ -0,0 +1,95 @@
+import {state, state_draw_card, state_is_deck_empty, state_is_player_pile_empty, state_pile_to_deck} from "./state.js";
+import {create_ghost_card_manual_move, ghost} from "./elements.js";
+import {TARGETS} from "./constants.js";
+import {handle_card_drop} from "./index.js";
+import {render_player_cards} from "./render.js";
+
+function on_deck_click() {
+    if (state_is_deck_empty()) {
+        if (state_is_player_pile_empty())
+            return;
+        state_pile_to_deck()
+    }
+
+    state_draw_card()
+    render_player_cards(state)
+}
+
+function on_pile_pointer_down(e) {
+    const card = e.target
+    const value = card.dataset.value;
+    const pile_id = card.parentElement.id;
+    const rect = card.getBoundingClientRect();
+
+    if (!value || !pile_id) return;
+
+    // Check if clicked on last/pickable card
+    const last_pile_value = state[pile_id].at(-1).split("-")[0];
+    if (last_pile_value !== value) return;
+
+    create_ghost_card_manual_move(card, {e_x : e.clientX, e_y: e.clientY, r_l: rect.left, r_t: rect.top});
+    window.addEventListener("pointermove", on_card_pointer_move);
+    window.addEventListener("pointerup", on_card_pointer_up);
+}
+
+function on_card_pointer_move(e) {
+    if (!ghost.el) return;
+
+    ghost.el.style.left = (e.clientX - ghost.offset_x) + "px";
+    ghost.el.style.top = (e.clientY - ghost.offset_y) + "px";
+}
+
+function on_card_pointer_down(e) {
+    const card = e.currentTarget;
+    const rect = card.getBoundingClientRect();
+
+    create_ghost_card_manual_move(card, {e_x : e.clientX, e_y: e.clientY, r_l: rect.left, r_t: rect.top});
+    window.addEventListener("pointermove", on_card_pointer_move);
+    window.addEventListener("pointerup", on_card_pointer_up);
+}
+
+function on_card_pointer_up(e) {
+    if (!ghost.el) return;
+    let src;
+    let target_type;
+    const target = e.target.id || e.target.parentElement?.id;
+
+    if (!target) console.error("Error: drop ID not found");
+
+    if (ghost.el.classList.contains("reserve_card"))
+        src = "player_reserve";
+    else if (ghost.el.classList.contains("main_card_one"))
+        src = "player_pile";
+    else if (ghost.el.classList.contains("pile_left_card") || ghost.el.classList.contains("pile_right_card"))
+        src = ghost.el.dataset.src;
+    else console.error(`Err: invalid ghost class {on_card_pointer_up} \n${ghost.el.classList}`);
+
+    if (e.target.parentElement.classList.contains("pile") || e.target.classList.contains("pile"))
+        target_type = TARGETS.pile;
+    else if (e.target.classList.contains("stack"))
+        target_type = TARGETS.stack;
+    else if (e.target.id === "opponent_pile")
+        target_type = TARGETS.opponent_pile;
+    else if (e.target === "opponent_reserve") {
+        target_type = TARGETS.opponent_reserve;
+    }
+    else console.error(`Err: invalid drop element class {on_card_pointer_up} \n${e.target?.classList}`);
+
+    if (src && target_type && target) {
+        handle_card_drop(src, target, target_type)
+    }
+
+    ghost.el.remove();
+    ghost.el = null;
+
+    window.removeEventListener("pointermove", on_card_pointer_move);
+    window.removeEventListener("pointerup", on_card_pointer_up);
+}
+
+export {
+    on_deck_click,
+    on_pile_pointer_down,
+    on_card_pointer_down,
+    on_card_pointer_up,
+    on_card_pointer_move,
+}
--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-import {pile_names, TARGETS} from "./constants.js"
+import {pile_names} from "./constants.js"
 import {render_opponent_cards, render_piles, render_player_cards, render_stacks} from "./render.js";
 import {is_valid_move} from "./validation.js";
 import {
     state,
-    state_draw_card,
-    state_is_deck_empty,
-    state_is_player_pile_empty,
     state_move_card,
-    state_pile_to_deck
 } from "./state.js";
 import {
     btn_create_room, btn_join_room,
     create_ghost_card_auto_move,
-    create_ghost_card_manual_move,
     el_player_card_area, el_player_deck_area,
     el_player_reserve, el_player_ws_status, el_room_id,
-    ghost, inp_room_id
+    inp_room_id
 } from "./elements.js";
 import {get_coordinates_for_move} from "./utils.js";
 import {ws_create_room, ws_join_room, ws_send} from "./websocket.js";
+import {on_card_pointer_down, on_deck_click, on_pile_pointer_down} from "./events.js";
 
 render_player_cards(state)
 render_opponent_cards(state)
@@ -57,77 +53,6 @@ el_pile_areas = null;
 el_main_card = null;
 el_reserve_card = null;
 
-function on_pile_pointer_down(e) {
-    const card = e.target
-    const value = card.dataset.value;
-    const pile_id = card.parentElement.id;
-    const rect = card.getBoundingClientRect();
-
-    if (!value || !pile_id) return;
-
-    // Check if clicked on last/pickable card
-    const last_pile_value = state[pile_id].at(-1).split("-")[0];
-    if (last_pile_value !== value) return;
-
-    create_ghost_card_manual_move(card, {e_x : e.clientX, e_y: e.clientY, r_l: rect.left, r_t: rect.top});
-    window.addEventListener("pointermove", on_card_pointer_move);
-    window.addEventListener("pointerup", on_card_pointer_up);
-}
-
-function on_card_pointer_down(e) {
-    const card = e.currentTarget;
-    const rect = card.getBoundingClientRect();
-
-    create_ghost_card_manual_move(card, {e_x : e.clientX, e_y: e.clientY, r_l: rect.left, r_t: rect.top});
-    window.addEventListener("pointermove", on_card_pointer_move);
-    window.addEventListener("pointerup", on_card_pointer_up);
-}
-
-function on_card_pointer_move(e) {
-    if (!ghost.el) return;
-
-    ghost.el.style.left = (e.clientX - ghost.offset_x) + "px";
-    ghost.el.style.top = (e.clientY - ghost.offset_y) + "px";
-}
-
-function on_card_pointer_up(e) {
-    if (!ghost.el) return;
-    let src;
-    let target_type;
-    const target = e.target.id || e.target.parentElement?.id;
-
-    if (!target) console.error("Error: drop ID not found");
-
-    if (ghost.el.classList.contains("reserve_card"))
-        src = "player_reserve";
-    else if (ghost.el.classList.contains("main_card_one"))
-        src = "player_pile";
-    else if (ghost.el.classList.contains("pile_left_card") || ghost.el.classList.contains("pile_right_card"))
-        src = ghost.el.dataset.src;
-    else console.error(`Err: invalid ghost class {on_card_pointer_up} \n${ghost.el.classList}`);
-
-    if (e.target.parentElement.classList.contains("pile") || e.target.classList.contains("pile"))
-        target_type = TARGETS.pile;
-    else if (e.target.classList.contains("stack"))
-        target_type = TARGETS.stack;
-    else if (e.target.id === "opponent_pile")
-        target_type = TARGETS.opponent_pile;
-    else if (e.target === "opponent_reserve") {
-        target_type = TARGETS.opponent_reserve;
-    }
-    else console.error(`Err: invalid drop element class {on_card_pointer_up} \n${e.target?.classList}`);
-
-    if (src && target_type && target) {
-        handle_card_drop(src, target, target_type)
-    }
-
-    ghost.el.remove();
-    ghost.el = null;
-
-    window.removeEventListener("pointermove", on_card_pointer_move);
-    window.removeEventListener("pointerup", on_card_pointer_up);
-}
-
 function handle_card_drop(src, target, target_type) {
     const card = state[src].at(-1)
 
@@ -140,17 +65,6 @@ function handle_card_drop(src, target, target_type) {
     render_opponent_cards(state);
 
     return true;
-}
-
-function on_deck_click() {
-    if (state_is_deck_empty()) {
-        if (state_is_player_pile_empty())
-            return;
-        state_pile_to_deck()
-    }
-
-    state_draw_card()
-    render_player_cards(state)
 }
 
 // Socket mock
@@ -196,7 +110,6 @@ function socket_behaviour_auto_move_card(src, target) {
     });
 }
 
-
 function socket_behaviour_update_state(state, src, target, id) {
     state_move_card(src, target);
 }
@@ -221,9 +134,9 @@ function readTest(data) {
     console.log("From socket", data);
 }
 
-
 export {
     ws_behaviour_set_player_connection,
     ws_behaviour_set_room,
-    readTest
+    readTest,
+    handle_card_drop
 }
